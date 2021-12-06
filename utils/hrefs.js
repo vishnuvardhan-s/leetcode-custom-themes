3a941f60181e713f22d86bdeab09d20515934db3
Vishnuvardhan S <greenmatrix22@gmail.com>
2021-12-06 18:09:20 +0530

    Added link

--- utils/hrefs.js
+++ utils/hrefs.js
@@ -1,5 +1,6 @@
 // used to get all the links of a website as an array of strings
 // paste this inside console of the website
+// https://codemirror.net/theme/
 var arr = [], l = document.links;
 for (var i = 0; i < l.length; i++) {
     arr.push(l[i].href);

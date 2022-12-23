36ac01279d6c44dabe39d2f1a005c79d4147446a
Vishnuvardhan S <greenmatrix22@gmail.com>
2022-12-23 13:12:17 +0530

    Custom Themes work

--- content.js
+++ content.js
@@ -2,9 +2,22 @@ async function applyUserTheme() {
   if (typeof monaco !== "undefined") {
     const ogSaveTheme = monaco.editor.setTheme;
     // hijack setTheme call from leetcode and apply custom theme
-    monaco.editor.setTheme = () => {
-      ogSaveTheme("hc-black");
-      monaco.editor.setTheme = ogSaveTheme;
+    monaco.editor.setTheme = async () => {
+      window.postMessage(
+        { type: "LEETCODE_CUSTOM_THEMES_REQUEST", theme: "Github Dark" },
+        "*"
+      );
+      window.addEventListener("message", (event) => {
+        if (
+          event.data.type &&
+          event.data.type === "LEETCODE_CUSTOM_THEMES_RESPONSE"
+        ) {
+          const theme = event.data.theme;
+          monaco.editor.defineTheme("github-dark", theme);
+          ogSaveTheme("github-dark");
+          monaco.editor.setTheme = ogSaveTheme;
+        }
+      });
     };
   } else {
     setTimeout(applyUserTheme, 250);
